b2c8fe5e9c334f457814972932f223f1b5d937a2
Siddharth Pandit <110214185+FlashSpeedie@users.noreply.github.com>
2024-12-30 17:49:16 -0600

    Update Sidebar.jsx

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -8,17 +8,29 @@ const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className="sidebar-options">
+        <NavLink to='/dashboard' className="sidebar-option">
+            <img src={assets.dashboard_icon} alt="" />
+            <p>Dashboard</p>
+        </NavLink>
         <NavLink to='/add'className="sidebar-option">
           <img src={assets.add_icon} alt="" />
           <p>Add Items</p>
         </NavLink>
         <NavLink to='/list' className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
+          <img src={assets.list_icon} alt="" />
           <p>List Items</p>
         </NavLink>
         <NavLink to='/orders' className="sidebar-option">
           <img src={assets.order_icon} alt="" />
           <p>Orders</p>
+        </NavLink>
+        <NavLink to='/sales' className="sidebar-option">
+          <img src={assets.report_icon} alt="" />
+          <p>Sales Report</p>
+        </NavLink>
+        <NavLink to='/message' className="sidebar-option">
+          <img src={assets.message_icon} alt="" />
+          <p>Messages</p>
         </NavLink>
       </div>
     </div>
