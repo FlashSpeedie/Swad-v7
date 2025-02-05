6be6e63860610e704341ff3c22fd38ed4946a388
Siddharth Pandit <siddharth20pandit@gmail.com>
2025-02-05 08:11:54 -0600

    fixes

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -7,6 +7,7 @@ import axios from 'axios';
 
 const Verify = () => {
 
+    console.log("Verify component mounted");
     const [searchParams,setSearchParams] = useSearchParams();
     const success = searchParams.get("success")
     const orderId = searchParams.get("orderId")
