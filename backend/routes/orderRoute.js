48d86a47b9d2676c634b5edeedbd2dad7226338e
Siddharth Pandit <siddharth20pandit@gmail.com>
2025-02-04 22:50:33 -0600

    e

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -2,6 +2,7 @@ import express from "express"
 import authMiddleware from './../middleware/auth.js';
 import { placeOrder, verifyOrder, userOrders,listOrders,updateStatus } from "../controllers/orderController.js";
 
+
 const orderRouter = express.Router();
 
 orderRouter.post("/place",authMiddleware,placeOrder);
