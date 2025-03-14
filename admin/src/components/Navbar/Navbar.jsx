ed79d9a6cf91947ee4e83a51343cd51857f059fb
Siddharth Pandit <siddharth20pandit@gmail.com>
2025-03-13 19:33:16 -0500

    Final 3

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -18,25 +18,23 @@ const Navbar = () => {
           <img src={assets.logo} alt="Logo" className="navbar-logo" />
         </a>
         <div>
-          <p className="navbar-logo-text">Swad</p>
+          <a href="/" className="navbar-logo-text">Swad</a>
           <p className="navbar-admin-text">Admin</p>
         </div>
       </div>
       
-      <div className="navbar-hamburger" onClick={toggleMenu}></div>
       <div className="navbar-hamburger" onClick={toggleMenu}>
         <div className={`hamburger-bar ${isMenuOpen ? 'active' : ''}`} />
         <div className={`hamburger-bar ${isMenuOpen ? 'active' : ''}`} />
         <div className={`hamburger-bar ${isMenuOpen ? 'active' : ''}`} />
       </div>
 
-      {/* Navbar Menu */}
       <div className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
         <a href="/" className={`navbar-item ${location.pathname === '/' ? 'active' : ''}`}>Dashboard</a>
-        <a href="/add" className={`navbar-item ${location.pathname === '/add-items' ? 'active' : ''}`}>Add Items</a>
-        <a href="/list" className={`navbar-item ${location.pathname === '/list-items' ? 'active' : ''}`}>List Items</a>
+        <a href="/add" className={`navbar-item ${location.pathname === '/add' ? 'active' : ''}`}>Add Items</a>
+        <a href="/list" className={`navbar-item ${location.pathname === '/list' ? 'active' : ''}`}>List Items</a>
         <a href="/orders" className={`navbar-item ${location.pathname === '/orders' ? 'active' : ''}`}>Orders</a>
-        <a href="/message" className={`navbar-item ${location.pathname === '/messages' ? 'active' : ''}`}>Messages</a>
+        <a href="/message" className={`navbar-item ${location.pathname === '/message' ? 'active' : ''}`}>Messages</a>
       </div>
     </div>
   );
