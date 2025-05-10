5dbef7f527786be073b998816ed653b14274f4d2
Siddharth Pandit <siddharth20pandit@gmail.com>
2025-05-10 16:49:49 -0500

    Update Reference_Page.jsx

--- frontend/src/pages/Reference_Page/Reference_Page.jsx
+++ frontend/src/pages/Reference_Page/Reference_Page.jsx
@@ -7,6 +7,7 @@ const ReferencePage = () => {
         <div>
             <ul className="content">
                 <h1>Reference Page</h1>
+                <h2>User Session</h2>
                 <p>While the production version of the site will feature a login/signup popup for users, the competition version includes an auto-login functionality to streamline access and demonstrate core features without authentication barriers.</p>
                 <p>
                     <h2>Admin Link</h2>
@@ -27,7 +28,7 @@ const ReferencePage = () => {
                             className="pdf-link"
                         >
                             Click Here</a> to Open Copyright & Plan of Work Checklist PDF if the PDF below doesn't load properly.
-                        
+
                     </li>
                 </ul>
                 <div>
