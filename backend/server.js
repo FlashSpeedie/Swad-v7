24de3fdbc7383291f60c444709f209fbd664762d
Siddharth Pandit <siddharth20pandit@gmail.com>
2025-05-19 20:38:21 -0500

    rateLimit

--- backend/server.js
+++ backend/server.js
@@ -8,34 +8,37 @@ import mongoose from "mongoose"
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
 import dotenv from 'dotenv';
+import rateLimit from 'express-rate-limit'
 
 // app config
 const app = express()
 const port = process.env.PORT || 4000;
-console.log('Cloudinary Config:', process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_API_KEY);
 dotenv.config();
 console.log('Cloudinary Config:', process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_API_KEY);
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 400,
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+app.use(limiter);
 
-// middleware
 app.use(express.json())
 app.use(cors())
 
-// db connect
 connectDB();
 
-// api endpoint
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
+app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))
 app.use("/api/user", userRouter)
-app.use("/api/cart",cartRouter) 
-app.use("/api/order",orderRouter)
+app.use("/api/cart", cartRouter) 
+app.use("/api/order", orderRouter)
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("API Working")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`)
 })
-
